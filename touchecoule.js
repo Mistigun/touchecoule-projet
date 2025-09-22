2b2d34343a7c4aa8c39c62fbd5a79f0088aa496b
Mistigun <ch.courcol@outlook.fr>
2025-09-22 20:12:15 +0200

    feat: ajout fonctions game/couler/tirer et la fin code

--- touchecoule.js
+++ touchecoule.js
@@ -26,3 +26,56 @@ function afficherGrille(grille: string[][], hideShips: boolean): void {
 function bateau(grille: string[][], x: number, y: number): void {
   grille[x][y] = "B";
 }
+function tirer(enemyGrid: string[][]): void {
+  let x: number, y: number;
+
+  while (true) {
+    const xs = prompt(`Entre X (0-${taille - 1})`) || "";
+    const ys = prompt(`Entre Y (0-${taille - 1})`) || "";
+
+    x = parseInt(xs);
+    y = parseInt(ys);
+
+    if (!isNaN(x) && !isNaN(y) && x >= 0 && x < taille && y >= 0 && y < taille) break;
+
+    alert("Coordonnées invalides !");
+  }
+
+  if (enemyGrid[x][y] === "B") {
+    enemyGrid[x][y] = "X";
+    alert(`Touché en (${x},${y}) !`);
+  } else if (enemyGrid[x][y] === "~") {
+    enemyGrid[x][y] = "O";
+    alert(`À l’eau en (${x},${y})...`);
+  } else {
+    alert(`Déjà tiré en (${x},${y}) !`);
+  }
+
+  afficherGrille(enemyGrid, true);
+}
+
+function couler(grille: string[][]): boolean {
+  for (let i = 0; i < taille; i++) {
+    for (let j = 0; j < taille; j++) {
+      if (grille[i][j] === "B") return false;
+    }
+  }
+  return true;
+}
+
+function game(): void {
+  const enemy = TABLEAU();
+
+ 
+  bateau(enemy, 1, 1);
+  bateau(enemy, 3, 4);
+
+  alert("Le jeu commence");
+  afficherGrille(enemy, true);
+
+  while (true) {
+    tirer(enemy);
+
+    
+}
+game();
